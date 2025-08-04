4fe8e1c95319049001873439996627dca3556311
muderick <mumiaderick@gmail.com>
2025-08-04 20:27:46 +0300

    imports fix

--- src/views/BookDetails_Location.jsx
+++ src/views/BookDetails_Location.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useLocation } from "react-router-dom";
 import { useTheme } from "../context/useTheme";
 import { Button, Col, Row, Tag, Typography } from "antd";
